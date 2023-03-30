eb3b1125ca8511201d206dfe7a7ec638cc8377e2
elbeejay <jhariharan@usgs.gov>
2023-03-30 12:46:30 -0400

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,14 @@
 """Unit tests for the utils module."""
 import pytest
+import pandas as pd
 from hyswap import utils
 
 
 def test_filter_approved_data():
     """Test the filter_approved_data function."""
     data = {"a": ["A", "A", "P", "P"], "b": [1, 2, 3, 4]}
-    df = utils.filter_approved_data(data, filter_column="a")
+    data_df = pd.DataFrame(data)
+    df = utils.filter_approved_data(data_df, filter_column="a")
     assert df["b"].tolist() == [1, 2]
     with pytest.raises(ValueError):
-        utils.filter_approved_data(data, filter_column=None)
+        utils.filter_approved_data(data_df, filter_column=None)
