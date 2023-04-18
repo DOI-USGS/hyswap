a52537ba6dfd27ea5d4d98b57247ef46b599ee59
elbeejay <jhariharan@usgs.gov>
2023-04-18 13:51:25 -0400

    split up raster hydrograph function into smaller ones

--- hyswap/rasterhydrograph.py
+++ hyswap/rasterhydrograph.py
@@ -46,9 +46,117 @@ def format_data(df, data_column_name, date_column_name=None,
 
     Examples
     --------
-    Formatting daily data for a raster hydrograph.
+    Formatting synthetic daily data for a raster hydrograph.
 
-    .. todo:: Add example.
+    .. doctest::
+
+        >>> df = pd.DataFrame({'date': pd.date_range('1/1/2010', '12/31/2010'),
+        ...                    'data': np.random.rand(365)})
+        >>> df_formatted = rasterhydrograph.format_data(df, 'data')
+        >>> df_formatted.index[0]
+        2010
+        >>> len(df_formatted.columns)
+        365
+
+    Formatting real daily data for a raster hydrograph.
+
+    .. doctest::
+        :skipif: True  # dataretrieval functions break CI pipeline
+
+        >>> df, _ = dataretrieval.nwis.get_dv(
+        ...     "03586500", parameterCd="00060",
+        ...     start="2000-01-01", end="2002-12-31")
+        >>> df_formatted = rasterhydrograph.format_data(df, '00060_Mean')
+        >>> df_formatted.index[0]
+        2000
+        >>> len(df_formatted.columns)
+        365
+
+    """
+    # check inputs
+    _check_inputs(df, data_column_name, date_column_name,
+                  data_type, year_type, begin_year, end_year)
+
+    # calculate the date range
+    df, date_range = _calculate_date_range(df, begin_year, end_year,
+                                           year_type, date_column_name)
+
+    # format date_range as YYYY-MM-DD
+    date_range = date_range.strftime('%Y-%m-%d')
+
+    # set data type
+    data_type = _set_data_type(data_type)
+
+    # make output data frame
+    # calculation of rolling mean is done on the data column
+    df_out = df[data_column_name].rolling(
+        data_type, center=True).mean().to_frame()
+
+    # convert date index to YYYY-MM-DD format
+    df_out.index = df_out.index.strftime('%Y-%m-%d')
+
+    # fill in missing dates as nan values to complete the years
+    df_out = df_out.reindex(index=date_range)
+
+    # convert date index to datetime format
+    df_out.index = pd.to_datetime(df_out.index)
+
+    # organize data frame
+    # rows are years and columns are days in the year
+    # add year column
+    df_out['year'] = df_out.index.year
+    if year_type == 'water':
+        df_out.loc[df_out.index.month < 10, 'year'] -= 1
+
+    # add day of year column
+    df_out['day'] = df_out.index.dayofyear
+    if year_type == 'water':
+        df_out.loc[df_out.index.month < 10, 'day'] += 274
+
+    # set index to year and day of year columns
+    df_out = df_out.pivot(index='year', columns='day', values=data_column_name)
+
+    # reverse order of the index so year order matches legacy Water Watch
+    df_out = df_out.iloc[::-1]
+
+    return df_out
+
+
+def _check_inputs(df, data_column_name, date_column_name,
+                  data_type, year_type, begin_year, end_year):
+    """Private function to check inputs for the format_data function.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The data to format. Must have a date column or the index must be the
+        date values.
+    data_column_name : str
+        The name of the column containing the data.
+    date_column_name : str, None
+        The name of the column containing the date or None if the index is
+        the date.
+    data_type : str
+        The type of data. Must be one of 'daily', '7-day', '14-day', and
+        '28-day'. If '7-day', '14-day', or '28-day' is
+        specified, the data will be averaged over the specified period. NaN
+        values will be used for any days that do not have data. If present,
+        NaN values will result in NaN values for the entire period.
+    year_type : str
+        The type of year to use. Must be one of 'calendar' or 'water'.
+        'calendar' starts the year on January 1 and ends on
+        December 31. 'water' starts the year on October 1 and ends on
+        September 30.
+    begin_year : int, None
+        The first year to include in the data. If None, the first year in
+        the data will be used.
+    end_year : int, None
+        The last year to include in the data. If None, the last year in the
+        data will be used.
+
+    Returns
+    -------
+    None
 
     """
     # check data type
@@ -95,19 +203,52 @@ def format_data(df, data_column_name, date_column_name=None,
                 raise ValueError('end_year must be less than or equal to the '
                                  'maximum year in the data')
 
+    return None
+
+
+def _calculate_date_range(df, begin_year, end_year, year_type,
+                          date_column_name):
+    """Private function to calculate the date range and set the index.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The data to format. Must have a date column or the index must be the
+        date values.
+    begin_year : int, None
+        The first year to include in the data. If None, the first year in
+        the data will be used.
+    end_year : int, None
+        The last year to include in the data. If None, the last year in the
+        data will be used.
+    year_type : str
+        The type of year to use. Must be one of 'calendar' or 'water'.
+        'calendar' starts the year on January 1 and ends on
+        December 31. 'water' starts the year on October 1 and ends on
+        September 30.
+    date_column_name : str, None
+        The name of the column containing the date or None if the index is
+        the date.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        The data with the date range set as the index.
+    date_range : pandas.DatetimeIndex
+        The date range.
+
+    """
+    # set date column as index
+    if date_column_name is not None:
+        df = df.set_index(date_column_name)
+
     # set begin year
     if begin_year is None:
-        if date_column_name is not None:
-            begin_year = df['date'].dt.year.min()
-        else:
-            begin_year = df.index.year.min()
+        begin_year = df.index.year.min()
 
     # set end year
     if end_year is None:
-        if date_column_name is not None:
-            end_year = df['date'].dt.year.max()
-        else:
-            end_year = df.index.year.max()
+        end_year = df.index.year.max()
 
     # set date range
     if year_type == 'calendar':
@@ -117,10 +258,29 @@ def format_data(df, data_column_name, date_column_name=None,
         date_range = pd.date_range(f'{begin_year}-10-01',
                                    f'{end_year}-09-30')
 
-    # formate date_range as YYYY-MM-DD
-    date_range = date_range.strftime('%Y-%m-%d')
+    return df, date_range
 
-    # set data type
+
+def _set_data_type(data_type):
+    """Private function to set the data type.
+
+    Parameters
+    ----------
+    data_type : str
+        The type of data. Must be one of 'daily', '7-day', '14-day', and
+        '28-day'. If '7-day', '14-day', or '28-day' is
+        specified, the data will be averaged over the specified period. NaN
+        values will be used for any days that do not have data. If present,
+        NaN values will result in NaN values for the entire period.
+
+    Returns
+    -------
+    data_type : str
+        The formatted frequency string to be used with
+        pandas.DataFrame.rolling to calculate the average over the correct
+        temporal period.
+
+    """
     if data_type == 'daily':
         data_type = 'D'
     elif data_type == '7-day':
@@ -130,33 +290,4 @@ def format_data(df, data_column_name, date_column_name=None,
     elif data_type == '28-day':
         data_type = '28D'
 
-    # make output data frame
-    # calculation of rolling mean is done on the data column
-    df_out = df[data_column_name].rolling(
-        data_type, center=True).mean().to_frame()
-
-    # convert date index to YYYY-MM-DD format
-    df_out.index = df_out.index.strftime('%Y-%m-%d')
-
-    # fill in missing dates as nan values to complete the years
-    df_out = df_out.reindex(index=date_range)
-
-    # convert date index to datetime format
-    df_out.index = pd.to_datetime(df_out.index)
-
-    # organize data frame
-    # rows are years and columns are days in the year
-    # add year column
-    df_out['year'] = df_out.index.year
-    if year_type == 'water':
-        df_out.loc[df_out.index.month < 10, 'year'] -= 1
-
-    # add day of year column
-    df_out['day'] = df_out.index.dayofyear
-    if year_type == 'water':
-        df_out.loc[df_out.index.month < 10, 'day'] += 274
-
-    # set index to year and day of year columns
-    df_out = df_out.pivot(index='year', columns='day', values=data_column_name)
-
-    return df_out
+    return data_type
