3da3c69771817313c24de7dabbab5c024ea997aa
elbeejay <jayaram.hariharan@utexas.edu>
2023-06-14 16:26:27 +0000

    Deploying to gh-pages from @ DOI-USGS/hyswap@36ef5429d14e03a252b64114f5eb15950a30eee4 🚀

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "hyswap 0.1.dev1+g4c3751d documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "hyswap 0.1.dev1+g36ef542 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
