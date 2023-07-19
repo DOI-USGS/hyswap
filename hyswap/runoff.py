fcea1c5ec3f70f169f722b7c75905206de963c21
elbeejay <jhariharan@usgs.gov>
2023-07-19 10:26:31 -0400

    geometric runoff calculation

--- hyswap/runoff.py
+++ hyswap/runoff.py
@@ -1,4 +1,5 @@
 """Runoff functions for hyswap."""
+import pandas as pd
 
 
 def convert_cfs_to_runoff(cfs, drainage_area, frequency="annual"):
@@ -95,3 +96,119 @@ def streamflow_to_runoff(df, data_col, drainage_area, frequency="annual"):
         lambda x: convert_cfs_to_runoff(x, drainage_area, frequency=frequency)
     )
     return df
+
+
+def calculate_geometric_runoff(geoid, df_list, weights_matrix,
+                               start_date=None, end_date=None,
+                               data_col='runoff'):
+    """Function to calculate the runoff for a specified geometry.
+
+    Parameters
+    ----------
+    geoid : str
+        Geometry ID for the geometry of interest.
+
+    df_list : list
+        List of dataframes containing runoff data for each site in the
+        geometry.
+
+    weights_matrix : pandas.DataFrame
+        DataFrame containing the weights for all sites and all geometries.
+        Columns are geometry IDs, index is site IDs.
+
+    start_date : str, optional
+        Start date for the runoff calculation. If not specified, the earliest
+        date in the df_list will be used. Format is 'YYYY-MM-DD'.
+
+    end_date : str, optional
+        End date for the runoff calculation. If not specified, the latest
+        date in the df_list will be used. Format is 'YYYY-MM-DD'.
+
+    data_col : str, optional
+        Column name containing runoff data in the dataframes in df_list.
+        Default is 'runoff', as it is assumed these dataframes are created
+        using the :obj:`streamflow_to_runoff` function.
+
+    Returns
+    -------
+    pandas.Series
+        Series containing the area-weighted runoff values for the geometry.
+    """
+    # get date range
+    date_range = _get_date_range(df_list, start_date, end_date)
+
+    # get site list from weights matrix index
+    site_list = weights_matrix.index.tolist()
+
+    # create empty dataframe to store results, index is site_list,
+    # columns are date_range
+    runoff_df = pd.DataFrame(index=site_list, columns=date_range)
+
+    # loop through the df_list to populate rows of the runoff_df
+    for df in df_list:
+        # get site id (assumed to be string from NWIS)
+        site_id = df['site_no'][0]
+        # convert site_id to int for indexing weights matrix
+        site_id = int(site_id)
+        # get weight for site
+        weight = weights_matrix[geoid].loc[site_id]
+        # get runoff for site
+        runoff = df[data_col]
+        # multiply weights by runoff
+        weighted_runoff = weight * runoff
+        # add weighted runoff to runoff_df
+        # pandas seems to use dates to automatically align data :)
+        runoff_df.loc[site_id] = weighted_runoff
+
+    # combine the new runoff_df with the existing weights matrix to calculate
+    # the area-weighted runoff values for the geometry
+    runoff_sum = runoff_df.sum(axis=0, skipna=True)
+    weights_sum = weights_matrix[geoid].sum(skipna=True)
+    weighted_runoff = runoff_sum / weights_sum
+
+    return weighted_runoff
+
+
+def _get_date_range(df_list, start_date, end_date):
+    """Get date range for runoff calculation.
+
+    This is an internal function used by the :obj:`calculate_geometric_runoff`
+    function to get the date range for the runoff calculation. If no start or
+    end date is specified, the earliest/latest date in the df_list will be
+    used.
+
+    Parameters
+    ----------
+    df_list : list
+        List of dataframes containing runoff data for each site in the
+        geometry.
+
+    start_date : str, optional
+        Start date for the runoff calculation. If not specified, the earliest
+        date in the df_list will be used. Format is 'YYYY-MM-DD'.
+
+    end_date : str, optional
+        End date for the runoff calculation. If not specified, the latest
+        date in the df_list will be used. Format is 'YYYY-MM-DD'.
+
+    Returns
+    -------
+    pandas.DatetimeIndex
+        DatetimeIndex containing the date range for the runoff calculation.
+    """
+    if start_date is None:
+        # if no start date iterate through df_list to find earliest date
+        start_date = df_list[0].index[0]
+        for df in df_list:
+            if df.index[0] < start_date:
+                start_date = df.index[0]
+    if end_date is None:
+        # if no end date iterate through df_list to find latest date
+        end_date = df_list[0].index[-1]
+        for df in df_list:
+            if df.index[-1] > end_date:
+                end_date = df.index[-1]
+    # create range of dates from start to end
+    date_range = pd.date_range(start_date, end_date).tz_localize('UTC')
+
+    return date_range
