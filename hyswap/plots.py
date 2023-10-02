a26353b3f8790745c2adadcaa876d5f47b782b46
Elise Hinman <ehinman@usgs.gov>
2023-10-02 11:08:33 -0400

    added URL for raster hydrograph ref

--- hyswap/plots.py
+++ hyswap/plots.py
@@ -150,9 +150,10 @@ def plot_raster_hydrograph(df_formatted, ax=None,
     Koehler, R. 2004. Raster Based Analysis and Visualization of Hydrologic
     Time Series. Ph.D  dissertation, University of Arizona. Tucson, AZ, 189 p.
 
-    Strandhagen, E., Marcus, W.A., and Meacham, J.E. 2006. Views of the
+    `Strandhagen, E., Marcus, W.A., and Meacham, J.E. 2006. Views of the
     rivers: representing streamflow of the greater Yellowstone ecosystem.
-    Cartographic Perspectives, no. 55, Fall.
+    Cartographic Perspectives, no. 55, 54-29.
+    <https://cartographicperspectives.org/index.php/journal/article/view/cp55-strandhagen-et-al/pdf>`__  # noqa: E501
 
     Parameters
     ----------
