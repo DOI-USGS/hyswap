70ace8088ab65d5b2217b9daa40fe74af9a68f38
lstanish-usgs <89864109+lstanish-usgs@users.noreply.github.com>
2023-11-09 22:09:09 +0000

    Deploying to gh-pages from @ DOI-USGS/hyswap@f39d874d9b82f3f22cec62fcbb0423d319caaebd 🚀

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "hyswap 0.1.dev1+g3f77fca documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "hyswap 0.1.dev1+gf39d874 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g3f77fca',
+    VERSION: '0.1.dev1+gf39d874',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
