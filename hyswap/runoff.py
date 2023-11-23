ba94053326135e17143009b9e6e545c679ca6ee3
Margaux Sleckman <msleckman@usgs.gov>
2023-11-22 17:51:05 -0800

    comment on assert

--- hyswap/runoff.py
+++ hyswap/runoff.py
@@ -283,7 +283,8 @@ def identify_sites_from_weights(geom_id,
     # Filter df to designated geometry (e.g. huc8)
     filtered_df = weights_df[weights_df[geom_id_col] == geom_id]
 
-    assert all(filtered_df[site_col].str.len() >= 8) == True, 'site numbers character length 8+ characters'
+    # Check that all sites ids have a character count of at least 8
+    assert all(filtered_df[site_col].str.len() >= 8), 'site numbers character length 8+ characters'
 
     # Check whether sites is the df index or not 
     if site_col == 'index':
