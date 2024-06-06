c5485a1f414e167bfdaba5eb3ea2423d93e88abe
Elise Hinman <ehinman@usgs.gov>
2024-06-06 07:33:14 -0500

    quick test with build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
 ]
 
 # allow errors to occur in notebooks
-nbsphinx_allow_errors = True
+# nbsphinx_allow_errors = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
